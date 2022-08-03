7ba60d70027e46b9eef2225329f8be7b8b8fba71
Rahul.BEE <rahul.b@diamond-developers.com>
2022-08-03 10:11:37 +0400

    Updated the banner and navbar

--- components/Homepage/Bannersection.jsx
+++ components/Homepage/Bannersection.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 const Bannersection = () => {
-  return <div style={{position:"relative", width:"100vw", height:"80vh"}}>
+  return <div style={{position:"relative", width:"100vw", height:"100vh"}}>
     <Image src="/Images/map-pic.png" layout='fill' objectFit='cover' objectPosition="50% 50%" quality={"100"}/>
     <div style={{position:"absolute", width:"100vw" , height:"100%",left:0, bottom:"-1px", display:"flex", justifyContent:"flex-start" ,alignItems:"flex-end"}}>
       <img src={"/Svg/Subtract.svg"} width="100%"/>
