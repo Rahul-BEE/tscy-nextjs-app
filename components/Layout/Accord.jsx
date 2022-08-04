cdc162d1d623093264f76b352b9f4420ed48de9e
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-08-04 14:23:58 +0400

    foooter Accord componet data add

--- components/Layout/Accord.jsx
+++ components/Layout/Accord.jsx
@@ -11,34 +11,36 @@ function CustomToggle({children, eventkey}){
     return (
         <div className={`${styles.mobile_dropdown} flex `}>
             <h4>{children}</h4>
-            <BsPlus onClick={decoratedOnClick} />
+            <BsPlus onClick={decoratedOnClick}  className={styles.icon}/>
         
         </div>
     )
 }
 
 
-function Accord() {
+function Accord({data}) {
   return (
     <div>
         
             <Accordion className={styles.accord}>
-                <Card className={styles.accord_card}>
-                    <Card.Header className={styles.bg_transparent}>
-                        <CustomToggle>About</CustomToggle>
-                    </Card.Header>
-                <Accordion.Collapse>
-                    <Card.Body className={styles.accord_card_body}>
-                    <ul>
-                <li>Reach out to us</li>
-                <li>Become a Broker</li>
-                <li>Language Support</li>
-                <li>Language Support</li>
-                <li>Help and Support</li>
-              </ul>
-                    </Card.Body>
-                </Accordion.Collapse>
-                </Card>
+                {data.map((e,index)=>(
+                                    <Card className={styles.accord_card}>
+                                    <Card.Header className={styles.bg_transparent}>
+                                        <CustomToggle eventkey={index}>{e.title}</CustomToggle>
+                                    </Card.Header>
+                                <Accordion.Collapse eventKey={index}>
+                                    <Card.Body className={styles.accord_card_body}>
+                                    <ul>
+                                        {e.links.map((value,index)=>(
+
+                                <li>{value.title}</li>
+                                        ))}
+                              </ul>
+                                    </Card.Body>
+                                </Accordion.Collapse>
+                                </Card>
+                ))}
+
             </Accordion>
 
     </div>
