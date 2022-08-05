98bc3f4f7b4bc405f3c18803a1ba3598e8dbee92
Rahul.BEE <rahul.b@diamond-developers.com>
2022-08-05 08:48:09 +0400

    Footer map keys added

--- components/Layout/Accord.jsx
+++ components/Layout/Accord.jsx
@@ -1,50 +1,48 @@
-import React from 'react'
-import {Accordion, Card, Container, useAccordionButton} from  "react-bootstrap"
-import {BsPlus} from 'react-icons/bs'
+import React from "react";
+import {
+  Accordion,
+  Card,
+  Container,
+  useAccordionButton,
+} from "react-bootstrap";
+import { BsPlus } from "react-icons/bs";
 import styles from "../../styles/layout.module.scss";
 
-
-function CustomToggle({children, eventkey}){
-    const decoratedOnClick = useAccordionButton(eventkey, ()=>
-        console.log('click')
-    );
-    return (
-        <div className={`${styles.mobile_dropdown} flex `}>
-            <h4>{children}</h4>
-            <BsPlus onClick={decoratedOnClick}  className={styles.icon}/>
-        
-        </div>
-    )
+function CustomToggle({ children, eventkey }) {
+  const decoratedOnClick = useAccordionButton(eventkey, () =>
+    console.log("click")
+  );
+  return (
+    <div className={`${styles.mobile_dropdown} flex `}>
+      <h4>{children}</h4>
+      <BsPlus onClick={decoratedOnClick} className={styles.icon} />
+    </div>
+  );
 }
 
-
-function Accord({data}) {
+function Accord({ data }) {
   return (
     <div>
-        
-            <Accordion className={styles.accord}>
-                {data.map((e,index)=>(
-                                    <Card className={styles.accord_card}>
-                                    <Card.Header className={styles.bg_transparent}>
-                                        <CustomToggle eventkey={index}>{e.title}</CustomToggle>
-                                    </Card.Header>
-                                <Accordion.Collapse eventKey={index}>
-                                    <Card.Body className={styles.accord_card_body}>
-                                    <ul>
-                                        {e.links.map((value,index)=>(
-
-                                <li>{value.title}</li>
-                                        ))}
-                              </ul>
-                                    </Card.Body>
-                                </Accordion.Collapse>
-                                </Card>
-                ))}
-
-            </Accordion>
-
+      <Accordion className={styles.accord}>
+        {data.map((e, index) => (
+          <Card className={styles.accord_card} key={`${index}_footer_link`}>
+            <Card.Header className={styles.bg_transparent}>
+              <CustomToggle eventkey={index}>{e.title}</CustomToggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={index}>
+              <Card.Body className={styles.accord_card_body}>
+                <ul>
+                  {e.links.map((value, index) => (
+                    <li>{value.title}</li>
+                  ))}
+                </ul>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        ))}
+      </Accordion>
     </div>
-  )
+  );
 }
 
-export default Accord
+export default Accord;
