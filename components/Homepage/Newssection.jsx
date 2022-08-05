9cc96b1d0c38baeb599098745e22071bd9782017
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-08-05 14:49:55 +0400

    layout for the news section

--- components/Homepage/Newssection.jsx
+++ components/Homepage/Newssection.jsx
@@ -1,4 +1,5 @@
 import styles from "../../styles/newssection.module.scss";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 function Newssection() {
   return (
@@ -14,10 +15,12 @@ function Newssection() {
         {/* main box */}
         <div className={styles.hero_box}>
           <div>
-            <h3>
-              Britain starts to open up, Ramadan begins. It feels like perfect
-              timing, with Russia cheif army scrubbling over the issue.
-            </h3>
+            <div className={styles.news_heading}>
+              <h3>
+                Britain starts to open up, Ramadan begins. It feels like perfect
+                timing, with Russia cheif army scrubbling over the issue.
+              </h3>
+            </div>
 
             <div className={styles.test_box}></div>
           </div>
@@ -26,11 +29,20 @@ function Newssection() {
         {/* small boxes */}
 
         <div className={styles.hero_secondry_box}>
-          <div>
+          <div className={styles.news_nav}>
+            <p>
+              <FaChevronLeft /> Previous
+            </p>
+            <p>
+              Next <FaChevronRight />
+            </p>
+          </div>
+          <div className={styles.secondry_box}>
             <div className={styles.test_box}></div>
+
             <h3>
-              Britain starts to open up, Ramadan begins. It feels like perfect
-              timing, with Russia cheif army scrubbling over the issue.
+              HOW THINGS ARE CHANGING WITH YOUTH ON THE STREETS ASKING FOR
+              SUSTAINABLE SOLUTIONS
             </h3>
           </div>
 
