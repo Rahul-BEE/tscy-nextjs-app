2ad9f99d279405981cc0763776843afde7f550cf
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-08-08 15:26:17 +0400

    news section mobile

--- components/Homepage/NewsSectionComponents/NewsMobile.jsx
+++ components/Homepage/NewsSectionComponents/NewsMobile.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import styles from "../../../styles/newssection.module.scss";
 
 function NewsMobile() {
-  return <div className={styles.hero_mobile_box}></div>;
+  return (
+    <div className={styles.hero_mobile_box}>
+      <div className={styles.test_box}>
+        <div className={styles.heading_mobile}>
+          <h2>ress and news</h2>
+          <p>Read here </p>
+        </div>
+
+        <div className={styles.bottom_text}>
+          <h3>
+            Britain starts to open up, Ramadan begins. It feels like perfect
+            timing, with Russia cheif army scrubbling over the issue.
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NewsMobile;
