2aa83294507cfadbb5e5809d777797514651f21a
Rahul.BEE <rahul.b@diamond-developers.com>
2022-08-09 10:00:02 +0400

    Sustainable feature section desktop version

--- components/Homepage/SustainableFeatures.jsx
+++ components/Homepage/SustainableFeatures.jsx
@@ -11,13 +11,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, FreeMode } from "swiper";
 import "swiper/css/autoplay";
 import "swiper/css/free-mode";
+import Image from "next/image";
 const SustainableFeatures = () => {
   const lan = useLanguage();
   const [featureSelected, setFeatureSelected] = useState(0);
   const [showData, setShowData] = useState(lan.sustainablesection.features[0]);
 
   useEffect(() => {
-    console.log("called");
     switch (featureSelected) {
       case 0: {
         setShowData(lan.sustainablesection.features[0]);
@@ -102,45 +102,48 @@ const SustainableFeatures = () => {
   const SustainableContents = () => {
     return (
       <Container fluid className={styles.sustainablecontentContainer}>
-        <div style={{ height: "32rem", overflow: "hidden" }}>
-          {showData && (
-            <>
-              <h3
-                style={{
-                  color:
-                    featureSelected === 1
-                      ? "#038fab"
-                      : featureSelected === 2
-                      ? "#70b795"
-                      : "#E79127",
-                }}>
-                {showData?.heading}
-              </h3>
-              <p className="px-4 my-4">{showData?.sub}</p>
-              <Swiper
-                className={styles.featureBullets}
-                modules={[Autoplay, FreeMode]}
-                direction="vertical"
-                slidesPerView={7}
-                loop={true}
-                freeMode={true}
-                speed={1000}
-                autoplay={{ delay: 1000 }}>
-                {showData?.bullets.map((bullet, index) => (
-                  <SwiperSlide key={`${index}_feature_bullet`}>
-                    <h5 className={styles.bulletSustainable}>{bullet}</h5>
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </>
-          )}
-        </div>
-        <div className={styles.featuresoverLay}></div>
+        <Row>
+          <Col style={{ position: "relative" }}>
+            <div style={{ height: "32rem", overflow: "hidden" }}>
+              {showData && (
+                <>
+                  <h3
+                    style={{
+                      color:
+                        featureSelected === 1
+                          ? "#038fab"
+                          : featureSelected === 2
+                          ? "#70b795"
+                          : "#E79127",
+                    }}>
+                    {showData?.heading}
+                  </h3>
+                  <p className="px-4 my-4">{showData?.sub}</p>
+                  <Swiper
+                    className={styles.featureBullets}
+                    modules={[Autoplay, FreeMode]}
+                    direction="vertical"
+                    slidesPerView={7}
+                    loop={true}
+                    freeMode={true}
+                    speed={1000}
+                    autoplay={{ delay: 1000 }}>
+                    {showData?.bullets.map((bullet, index) => (
+                      <SwiperSlide key={`${index}_feature_bullet`}>
+                        <h5 className={styles.bulletSustainable}>{bullet}</h5>
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                </>
+              )}
+            </div>
+            <div className={styles.featuresoverLay}></div>
+          </Col>
+        </Row>
       </Container>
     );
   };
 
-  const SustainableImageComponents = (data) => {};
   return (
     <div className={styles.app__sustainablefeaturesection}>
       <Row>
@@ -152,16 +155,27 @@ const SustainableFeatures = () => {
         </Col>
       </Row>
       <Row>
-        <Col className={styles.sustainablefeatureImages}>images</Col>
-        <Col className={styles.sustainablefeaturecontent} md={6}>
-          <Container className={`${styles.sustainablefeatures}`}>
+        <Col md={3} className={styles.sustainImgCol}>
+          <Image
+            src="/Images/sustainableleft.png"
+            width={562}
+            height={546}
+            className={styles.susimage1}
+          />
+        </Col>
+        <Col className={styles.sustainablefeaturecontent} md={6} sm={6} lg={6}>
+          <Container className={`${styles.sustainablefeatures}`} fluid>
             <SustainableIcons />
             {showData && <SustainableContents />}
           </Container>
         </Col>
-        <Col
-          className={`${styles.sustainablefeatureImages} ${styles.sustainablefeatureImagesposition}`}>
-          images
+        <Col md={3} className={styles.sustainImgCol}>
+          <Image
+            src="/Images/sustainableright.png"
+            width={470}
+            height={501}
+            className={styles.susimage2}
+          />
         </Col>
       </Row>
     </div>
