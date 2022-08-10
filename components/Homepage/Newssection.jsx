302b21e85cf87d7ad77215781bfe0ff018f7cc3f
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-08-10 17:14:01 +0400

    arabic

--- components/Homepage/Newssection.jsx
+++ components/Homepage/Newssection.jsx
@@ -17,22 +17,12 @@ function Newssection() {
     },
     {
       id: 2,
-      title: `2Britain starts to open up, Ramadan begins. It feels like
-      perfect timing, with Russia cheif army scrubbling over the
-      issue.`,
-      discription: `Thus, when using the definite article, the speaker assumes the
-      listener knows the identity of the noun’s referent (because it
-      is obvious, because it is common knowledge, or because it was
-      mentioned in the same sentence or an earlier sentence). Use of
-      an indefinite article implies that the speaker assumes the
-      listener does not have to be told the identity of the referent.
-      Use of an indefinite article implies that the speaker assumes
-      the listener does not have to be told the identity of the
-      referent.`,
+      title: lan.newssection.post[1].title,
+      discription: lan.newssection.post[1].discription,
     },
     {
       id: 3,
-      title: `3Britain starts to open up, Ramadan begins. It feels like
+      title: `Britain starts to open up, Ramadan begins. It feels like
       perfect timing, with Russia cheif army scrubbling over the
       issue.`,
       discription: `Thus, when using the definite article, the speaker assumes the
@@ -44,26 +34,6 @@ function Newssection() {
       Use of an indefinite article implies that the speaker assumes
       the listener does not have to be told the identity of the
       referent.`,
-    },
-    {
-      title: `4Britain starts to open up, Ramadan begins. It feels like
-      perfect timing, with Russia cheif army scrubbling over the
-      issue.`,
-    },
-    {
-      title: `5Britain starts to open up, Ramadan begins. It feels like
-      perfect timing, with Russia cheif army scrubbling over the
-      issue.`,
-    },
-    {
-      title: `6Britain starts to open up, Ramadan begins. It feels like
-      perfect timing, with Russia cheif army scrubbling over the
-      issue.`,
-    },
-    {
-      title: `7Britain starts to open up, Ramadan begins. It feels like
-      perfect timing, with Russia cheif army scrubbling over the
-      issue.`,
     },
   ];
 
