83f3c34a986fa26d3ec72a6102b0a777c02a481e
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-09-27 11:31:40 +0400

    location image change and path svg

--- components/Homepage/Location.jsx
+++ components/Homepage/Location.jsx
@@ -4,6 +4,7 @@ import { Row, Col } from "react-bootstrap";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Clickme from "../../public/Svg/locationbtn.svg";
+import LocationSvgComp from "./LocationSvg/LocationSvgComp";
 const Location = () => {
   const lan = useLanguage();
   const [show, setShow] = useState(false);
@@ -26,10 +27,10 @@ const Location = () => {
           {lan.locationsection.description}
         </Col>
       </Row>
-      <div className={styles.locationImagecontainer}>
+      {/* <div className={styles.locationImagecontainer}>
         {show ? (
           <Image
-            src="/Images/locationimage2.png"
+            src="/Images/location/Assets1.png"
             width={1728}
             height={652}
             layout="responsive"
@@ -43,6 +44,17 @@ const Location = () => {
           />
         )}
         {!show && <Clickme onClick={() => setShow(true)} />}
+      </div> */}
+
+      <div className={styles.locationImagecontainer1}>
+        <Image
+          src="/Images/location/Asset1.png"
+          width={1728}
+          height={872}
+          layout="responsive"
+          // layout="fill"
+        />
+        <LocationSvgComp />
       </div>
     </div>
   );
