30dd370bd76f86419fb24377ba37f58d93f5846b
Rahul.BEE <rahul.b@diamond-developers.com>
2022-10-07 16:33:35 +0400

    design ready

--- components/Villapage/SliderComponent.jsx
+++ components/Villapage/SliderComponent.jsx
@@ -1,23 +1,50 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styles from "../../styles/othervillas.module.scss";
 import useLanguage from "../../utils/useLanguage";
-
+import { motion } from "framer-motion";
+import { useRef } from "react";
+import { useEffect } from "react";
 const SliderComponent = () => {
   const lan = useLanguage();
   const router = useRouter();
   const { villaId } = router.query;
+  const carouselRef = useRef();
+  const [containerWidth, setContainerWidth] = useState(0);
   const data = lan.villaplansection.villas.find(
     (villa) => villa.slug === villaId
   );
+  const setNewComponentWidth = useCallback(() => {
+    setContainerWidth(carouselRef.current?.getBoundingClientRect().width);
+  }, [carouselRef]);
+
+  useEffect(() => {
+    setNewComponentWidth();
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", setNewComponentWidth);
+    }
+    return () => {
+      window.removeEventListener("resize", setNewComponentWidth);
+    };
+  }, [carouselRef.current]);
   return (
-    <div className={styles.sliderItemContainer}>
+    <motion.div
+      className={styles.sliderItemContainer}
+      drag="x"
+      onLoad={setNewComponentWidth}
+      ref={carouselRef}
+      dragConstraints={{
+        right: 0,
+        left: -1830,
+      }}>
       {data &&
         data.interior.map((item, index) => (
           <div className={styles.sliderItem} key={index}>
-            <h5>{item.title}</h5>
-            <p>{item.description}</p>
+            <div className={styles.sliderItemHeader}>
+              <h5>{item.title}</h5>
+              <p>{item.description}</p>
+            </div>
             <div className={styles.imgcontainer}>
               <Image
                 src={item.img}
@@ -26,9 +53,15 @@ const SliderComponent = () => {
                 objectPosition={"center"}
               />
             </div>
+            <div
+              style={{
+                position: "absolute",
+                inset: 0,
+                cursor: "grab",
+              }}></div>
           </div>
         ))}
-    </div>
+    </motion.div>
   );
 };
 
