2fc195cd523e2016b8f7ced588359b3bf84d8a0b
Rahul.BEE <rahul.b@diamond-developers.ae>
2022-10-20 11:42:33 +0400

    footer accord icon changed

--- components/Layout/Accord.jsx
+++ components/Layout/Accord.jsx
@@ -4,12 +4,13 @@ import Card from "react-bootstrap/Card";
 import useLanguage from "../../utils/useLanguage";
 import styles from "../../styles/layout.module.scss";
 import Link from "next/link";
-import { AiOutlinePlus } from "react-icons/ai";
-function CustomToggle({ children, eventKey }) {
-  const decoratedOnClick = useAccordionButton(eventKey, () =>
-    console.log("totally custom!")
-  );
-
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { useState } from "react";
+function CustomToggle({ children, eventKey, callback }) {
+  const decoratedOnClick = useAccordionButton(eventKey, () => {
+    console.log(eventKey);
+    callback(eventKey);
+  });
   return (
     <div
       type="button"
@@ -23,6 +24,14 @@ function CustomToggle({ children, eventKey }) {
 function Accord() {
   const lan = useLanguage();
   const data = lan.footer;
+  const [isOpen, setIsOpen] = useState(null);
+  const handleClick = (id) => {
+    if (isOpen === id) {
+      setIsOpen(null);
+      return;
+    }
+    setIsOpen(id);
+  };
   return (
     <Accordion className={styles.footeraccord}>
       {data && (
@@ -32,8 +41,9 @@ function Accord() {
             borderTop: "2px solid rgba(119, 119, 119, 0.2)",
           }}>
           <Card.Header className={styles.accordcardheader}>
-            <CustomToggle eventKey="0">
-              {data[0].title} <AiOutlinePlus />
+            <CustomToggle eventKey="0" callback={handleClick}>
+              {data[0].title}{" "}
+              {isOpen === "0" ? <AiOutlineMinus /> : <AiOutlinePlus />}
             </CustomToggle>
           </Card.Header>
           <Accordion.Collapse eventKey="0">
@@ -53,8 +63,9 @@ function Accord() {
       {data && (
         <Card className={styles.accordcard}>
           <Card.Header className={styles.accordcardheader}>
-            <CustomToggle eventKey="1">
-              {data[1].title} <AiOutlinePlus />
+            <CustomToggle eventKey="1" callback={handleClick}>
+              {data[1].title}{" "}
+              {isOpen === "1" ? <AiOutlineMinus /> : <AiOutlinePlus />}
             </CustomToggle>
           </Card.Header>
           <Accordion.Collapse eventKey="1">
@@ -74,8 +85,9 @@ function Accord() {
       {data && (
         <Card className={styles.accordcard}>
           <Card.Header className={styles.accordcardheader}>
-            <CustomToggle eventKey="2">
-              {data[2].title} <AiOutlinePlus />
+            <CustomToggle eventKey="2" callback={handleClick}>
+              {data[2].title}{" "}
+              {isOpen === "2" ? <AiOutlineMinus /> : <AiOutlinePlus />}
             </CustomToggle>
           </Card.Header>
           <Accordion.Collapse eventKey="2">
