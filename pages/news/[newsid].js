fe8ef50e3a99b1b330afb805b11b2a17984ef2e1
iqrailyas0108 <110390627+iqrailyas0108@users.noreply.github.com>
2022-10-25 12:25:24 +0400

    Bug Fix2

--- pages/news/[newsid].js
+++ pages/news/[newsid].js
@@ -46,7 +46,7 @@ const News = () => {
                     </div>
                     <div className={styles.mt22}>
                       <span className={styles.datestyling}>{data.date}</span>
-                      <div style={{ textAlign: "-webkit-center" }}>
+                      <div className={styles.centeraligned}>
                         <button className={styles.btn_blue}>
                           <BiShare
                             size={16}
