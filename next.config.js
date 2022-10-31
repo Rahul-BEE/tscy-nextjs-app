231f62fcc93a2229ac81df06d4eaaf3c11a0c113
Rahul.BEE <rahul.b@diamond-developers.ae>
2022-10-31 08:51:36 +0400

    Added the env var for variable

--- next.config.js
+++ next.config.js
@@ -18,6 +18,12 @@ const nextConfig = {
     locales: ["en", "ar"],
     defaultLocale: "en",
   },
+  env: {
+    NEXT_PUBLIC_EMAIL_JS_PUBLIC_KEY: "A0OhZGlGQnSbuFer8",
+    NEXT_PUBLIC_EMAIL_JS_SERVICE_ID: "service_p7b4tsl",
+    NEXT_PUBLIC_BROKER_EMAIL_TEMPLATE_ID: "template_bs7cvpn",
+    NEXT_PUBLIC_REGISTER_INTEREST_TEMPLATE_ID: "template_q9yq3pa",
+  },
 };
 
 module.exports = nextConfig;
