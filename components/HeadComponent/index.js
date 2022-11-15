aa7b56f09e6ce0006c67a842a8622363a67dbad8
Rahul.BEE <rahul.b@diamond-developers.ae>
2022-11-15 08:38:56 +0400

    Schema updated

--- components/HeadComponent/index.js
+++ components/HeadComponent/index.js
@@ -36,43 +36,28 @@ const HeadComponent = ({
       <meta name="twitter:image" content={og.img} />
       <meta name="twitter:site" content="@TSCYiti" />
       <script type="application/ld+json">
-        {`{ 
-          "@context": "https://schema.org",
-          "@type": "Organization",
-          "name": "The Sustainable City Yiti",
-          "legalName" : "The Sustainable City Yiti, Oman",
-          "image": "https://www.thesustainablecity-yiti.com/Images/masterplanimage.png",
-           "url": "https://thesustainablecity-yiti.com/",
-          "logo": "https://thesustainablecity-yiti.com/Logos/tsc-logo.svg",
-          "foundingDate": "2022",
-          "founders": [
-          {
-          "@type": "company",
-          "name": "Diamond Developers"
-          },
-          {
-          "@type": "company",
-          "name": "Omran"
-          } ],
-        "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "Al Bahri Rd, Muscat, Oman",
-        "addressLocality": "Al Bahri Rd, مسقط",
-        "addressRegion": "Yiti",
-        "addressCountry": "OMAN"
-        },
-        "contactPoint": {
-        "@type": "ContactPoint",
-        "contactType": "customer support",
-        "telephone": "[+96880003333]",
-        "email": "hello@thesustainablecity-yiti.com"
-        },
-    "sameAs": [ 
-    "https://www.linkedin.com/company/the-sustainable-city-yiti/",
-    "https://twitter.com/TSCYiti?s=20&t=YSRqGJh4w6nxJuNR23i_rw",
-    "https://www.facebook.com/ThesustainablecityYiti?ref=py_c",
-    "https://www.instagram.com/thesustainablecity_yiti/",
-    ]
+        {`{
+  "@context": "http://www.schema.org",
+  "@type": "Organization",
+  "name": "The Sustainable City -Yiti",
+  "url": "https://thesustainablecity-yiti.com/",
+  "logo": "https://thesustainablecity-yiti.com/Logos/tsc-logo.svg",
+  "image": "https://thesustainablecity-yiti.com/Images/masterplanimage.png",
+  "description": "Diamond Developers and Omran have joined forces to develop a world-class project that meets the highest standards of social, environmental, and economic sustainability.",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Mina Al Sultan Qaboos Centre Yiti",
+    "addressLocality": "Yiti",
+    "addressRegion": "Yiti",
+    "addressCountry": "Oman"
+  },
+  "hasMap": "https://www.google.com/maps/place/The+Sustainable+City+-+Yiti+Experience+Center+(TSCY)/@23.6254328,58.5788909,17z/data=!3m1!4b1!4m5!3m4!1s0x3e91f7fd475123d5:0xdf8ff3ebfaec4d72!8m2!3d23.6254328!4d58.5788909",
+  "openingHours": "Mo, Tu, We, Th, Fr 08:30-17:30",
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+968 800 33 33",
+    "contactType": "general"
+  }
 }`}
       </script>
       {children}
